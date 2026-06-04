a80d5801e9224f98ec57ee54779cfb2a59bd8e13
evansmith14 <184949993+evansmith14@users.noreply.github.com>
2026-06-04 18:54:19 +0500

    updated baseurl to staging

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api-dev.resvor.com',
+        target: 'https://api-staging.resvor.com',
         changeOrigin: true,
         credentials: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
