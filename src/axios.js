4622cad8d6e82bc4a80eb07b31baf455657c71a7
zackcole <183650880+zackcole@users.noreply.github.com>
2026-07-06 15:12:40 +0500

    update:push for main

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import { ErrorToast } from "./components/global/Toaster"; // Import your toaster
 import Cookies from "js-cookie";
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
-export const baseUrl = "https://api-dev.resvor.com";
+export const baseUrl = "https://api-staging.resvor.com";
 
 async function getDeviceFingerprint() {
   const fp = await FingerprintJS.load();
