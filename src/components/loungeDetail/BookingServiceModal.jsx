2a91b00fc2f793965b852dab950c63333a496f87
steveparker123 <176498134+steveparker123@users.noreply.github.com>
2026-07-08 18:55:53 +0500

    update: email validation work

--- src/components/loungeDetail/BookingServiceModal.jsx
+++ src/components/loungeDetail/BookingServiceModal.jsx
@@ -191,7 +191,7 @@ const BookingServiceModal = ({
                   value={instruction}
                   onChange={(e) => setInstruction(e.target.value)}
                   placeholder="Add here"
-                  maxLength={2000}
+                  maxLength={250}
                   className={`w-full px-4 py-2 text-sm rounded-[15px] bg-transparent ring-1 ring-[#CACACA] 
                           focus:ring-2 focus:ring-gray-200 focus:outline-none pr-12 placeholder:font-light placeholder:text-[12px] placeholder:text-[#727272] `}
                 ></textarea>
@@ -223,8 +223,8 @@ const BookingServiceModal = ({
           />
         )}
         {floorPlanModal && (
-          <FloorPlanModal 
-            onClose={() => setFloorPlanModal(false)} 
+          <FloorPlanModal
+            onClose={() => setFloorPlanModal(false)}
             floorPlan={floorPlan}
             availableTables={availableTables}
           />
